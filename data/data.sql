9aca66cdcd054b8d98bfaa55f32f12b9fe56c00e
lbeckman314 <lbeckman314@gmail.com>
2019-04-18 21:35:39 -0700

    Added minor comments.

--- data/data.sql
+++ data/data.sql
@@ -1,6 +1,7 @@
+-- *nix versions:
 -- Country
 \copy Country(Name,Country_Code) FROM 'data/country.csv' DELIMITER ',' CSV;
-
+-- City
 \copy city(id, name, country, lat, lon) FROM 'data/city.csv' DELIMITER ',' CSV;
 
 -- Windows versions:
