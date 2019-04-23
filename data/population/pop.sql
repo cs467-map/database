d941acddfc042922a1656947db19003c479ded9c
lbeckman314 <lbeckman314@gmail.com>
2019-04-23 14:33:34 -0700

    Added longitude check for population table.

--- data/population/pop.sql
+++ data/population/pop.sql
@@ -2,7 +2,8 @@
 SELECT C.id, TRUNC((pop_min + pop_max) / 2.0, 1) AS pop_avg
 FROM ne_10m_populated_places NE
 INNER JOIN City C ON (C.Name = NE.name)
-WHERE TRUNC(C.lat, 2) = TRUNC(NE.latitude, 2)
+WHERE TRUNC(C.lat, 2) = TRUNC(NE.latitude, 2) AND
+TRUNC(C.lon, 2) = TRUNC(NE.longitude, 2)
 
 --NE.name, NE.latitude, NE.longitude, C.lat, C.lon
 --ORDER BY pop_avg DESC;
