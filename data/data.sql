91f13181a9f6ec70e8112b53d2a1c461e4d1e8d1
lbeckman314 <lbeckman314@gmail.com>
2019-04-25 17:14:05 -0700

    Readded population data input.

--- data/data.sql
+++ data/data.sql
@@ -27,9 +27,11 @@ INNER JOIN temp_internet TI ON (TI.Country = C.name);
 
 -- Population Totals
 -- Build populations for all cities.
---\i data/population/population.sql
+\i data/population/population.sql
 -- Input those populations into the "City" table.
---\i data/population/pop.sql
+-- \i data/population/pop.sql
+\copy Population(CityId,Total) FROM 'data/population/population-total.csv' DELIMITER ',' CSV HEADER;
 
 
 -- Windows versions: replace all '/' above with '\'.
+-- %s:\:\\:g
