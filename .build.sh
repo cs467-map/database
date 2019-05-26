d48d18a344aff7c13fc9fcdb420f134e4ac8ff3a
lbeckman314 <lbeckman314@gmail.com>
2019-05-25 17:25:56 -0700

    Update build file for data export.

--- .build.sh
+++ .build.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 set +x
 
+src=/var/www/voyager-index-database
+data=voyager-index-data
+production=/var/www/pkgs/$data
+
 # update database
 
-GIT=voyager-index-database
-
-sudo su git
-cd $HOME/Documents/$GIT
-git pull origin master
-
+cd $src
+git pull -Xtheirs origin master
 psql -d map -f database.sql -f export-csv.sql
-
-exit
 
 # export data
 
-project=voyager-index-data
-production=/var/www/pkgs/$project
-
 sha256sum *.csv > sha256sums.txt
-gpg --pinentry-mode loopback --passphrase $gpgpass --batch --yes --detach-sign -a sha256sums.txt
+gpg --passphrase $gpgpass --batch --yes --detach-sign -a sha256sums.txt
 
 cp *.csv sha256sums.txt* $production
