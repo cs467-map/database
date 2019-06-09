acab4200161cc35ab71ec7b87e85efa3f22acef8
lbeckman314 <lbeckman314@gmail.com>
2019-06-09 15:05:47 -0700

    Update build script.

--- .build.sh
+++ .build.sh
@@ -4,18 +4,23 @@ set +x
 src=/var/www/voyager-index-database
 data=voyager-index-data
 production=/var/www/pkgs/$data
-files=voyager-index-data.* sha256sums.txt*
+files=voyager-index-data.voyager-index-data.json sha256sums.txt*
 
 # update database
 
+echo "updating database"
 cd $src
 git pull -Xtheirs origin master
+
+echo "exporting database"
 psql -d map -f database.sql -f export-csv.sql
 psql -d map -f database.sql -f export-json.sql
 
 # export data
 
+echo "signing exports"
 sha256sum *.csv > sha256sums.txt
 gpg --passphrase $gpgpass --batch --yes --detach-sign -a sha256sums.txt
 
+echo "copying exports"
 cp $files $production
